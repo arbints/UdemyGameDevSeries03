afc3d1f20b6785a537502d0835b55a68344760d6
JTWoodson <jingtianli.animation@gmail.com>
2022-06-27 12:17:08 -0500

    10_Structure_the_Joystick_and_Touch_Events

---
+++ Assets/Prefabs/UI/JoyStick/JoyStick.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoyStick : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log($"On Drag Fired {eventData.position}");  
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("PointerDown");
+  
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("PointerUp");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
