7450507dce8a8dedb7893d217c1efb8dcc638f1e
JTWoodson <jingtianli.animation@gmail.com>
2022-06-28 11:47:54 -0500

    11_Move_the_Joystick_with_Touch_Input

--- Assets/Prefabs/UI/JoyStick/JoyStick.cs
+++ Assets/Prefabs/UI/JoyStick/JoyStick.cs
@@ -5,21 +5,29 @@ using UnityEngine.EventSystems;
 
 public class JoyStick : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
 {
+    [SerializeField] RectTransform ThumbStickTrans;
+    [SerializeField] RectTransform BackgroundTrans;
+    [SerializeField] RectTransform CenterTrans;
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log($"On Drag Fired {eventData.position}");  
+        Debug.Log($"On Drag Fired {eventData.position}");
+        Vector2 TouchPos = eventData.position;
+        Vector2 centerPos = BackgroundTrans.position;
 
+        Vector2 localOffset = Vector2.ClampMagnitude(TouchPos - centerPos, BackgroundTrans.sizeDelta.x/2);
+        ThumbStickTrans.position = centerPos + localOffset;
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("PointerDown");
-  
+        BackgroundTrans.position = eventData.position;
+        ThumbStickTrans.position = eventData.position;
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        Debug.Log("PointerUp");
+        BackgroundTrans.position = CenterTrans.position;
+        ThumbStickTrans.position = BackgroundTrans.position;
     }
 
     // Start is called before the first frame update
