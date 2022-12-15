2433b818beb02d78f3be3402d9f46f0628cc9202
JTAline <jingtianli.animation@gmail.com>
2022-12-15 13:57:16 -0600

    43_Create_the_Node_Class

---
+++ Assets/Prefabs/Framework/AI/BehaviorTree/BTNode.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum NodeResult
+{
+    Success,
+    Failure,
+    Inprogress
+}
+
+public abstract class BTNode
+{
+    public NodeResult UpdateNode()
+    {
+        //one off thing
+        if(!started)
+        {
+            started = true;
+            NodeResult execResult = Execute();
+            if(execResult != NodeResult.Inprogress)
+            {
+                EndNode();
+                return execResult;
+            }
+        }
+
+        //time based
+        NodeResult updateResult = Update();
+        if(updateResult != NodeResult.Inprogress)
+        {
+            EndNode();
+        }
+        return updateResult;
+    }
+
+    //override in child class
+    protected virtual NodeResult Execute()
+    {
+        //one off thing
+        return NodeResult.Success;
+    }
+
+    protected virtual NodeResult Update()
+    {
+        //time based
+        return NodeResult.Success;
+    }
+
+    protected virtual void End()
+    {
+        //clean up
+    }
+
+    private void EndNode()
+    {
+        started = false;
+        End();
+    }
+
+    bool started = false;
+}
