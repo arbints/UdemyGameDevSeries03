73203bf603cf09d54100e3202818134e225e6448
JTAline <jingtianli.animation@gmail.com>
2022-12-16 11:10:57 -0600

    47 create the blackboard

--- Assets/Prefabs/Framework/AI/BehaviorTree/BehaviorTree.cs
+++ Assets/Prefabs/Framework/AI/BehaviorTree/BehaviorTree.cs
@@ -6,6 +6,12 @@ using UnityEngine;
 public abstract class BehaviorTree : MonoBehaviour
 {
     BTNode Root;
+    Blackboard blackboard = new Blackboard();
+
+    public Blackboard Blackboard
+    {
+        get { return blackboard; }
+    }
     // Start is called before the first frame update
     void Start()
     {
