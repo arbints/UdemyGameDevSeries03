33c7fdb28917f20bcf3eda181101da3ce9a83d2e
JTAline <jingtianli.animation@gmail.com>
2022-12-17 09:01:00 -0600

    the decorator classes

---
+++ Assets/Prefabs/Framework/AI/BehaviorTree/Decorator.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Decorator : BTNode
+{
+    BTNode child;
+
+    protected BTNode GetChild()
+    {
+        return child;
+    }
+
+    public Decorator(BTNode child)
+    {
+        this.child = child;
+    }
+}
