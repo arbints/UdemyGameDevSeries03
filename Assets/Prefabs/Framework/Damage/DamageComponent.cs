1bee86b76a4b603074228cbe45fac8ab99bbda1a
Arbint <cg.jingtianli@gmail.com>
2023-02-10 07:20:27 -0600

    87_Fire_Ability_Base_Classes

--- Assets/Prefabs/Framework/Damage/DamageComponent.cs
+++ Assets/Prefabs/Framework/Damage/DamageComponent.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class DamageComponent : MonoBehaviour
+public abstract class DamageComponent : MonoBehaviour, ITeamInterface
 {
     [SerializeField] protected bool DamageFriendly;
     [SerializeField] protected bool DamageEnemy;
     [SerializeField] protected bool DamageNeutral;
     ITeamInterface teamInterface;
+    public int GetTeamID()
+    {
+        if(teamInterface != null)
+            return teamInterface.GetTeamID();
+        return -1;
+    }
 
     public void SetTeamInterfaceSrc(ITeamInterface teamInterface)
     {
